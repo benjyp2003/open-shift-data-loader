81f562c22b59b9a83d51559c03705ef16231cb80
benjyp2003 <benjypfeffer@gmail.com>
2025-08-14 17:20:42 +0300

    Created sql insert_data.sql file that inserts data into a table.

---
+++ scripts/insert_data.sql
@@ -0,0 +1,6 @@
+insert into table1 (id, first_name, last_name)
+    values (1, 'Alice',   'Smith'),;
+           (2, 'Bob', 'Johnson'),
+           (3, 'Charlie', 'Brown'),
+           (4, 'Diana', 'White'),
+           (5, 'Ethan', 'Black');
